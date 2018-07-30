cf6b5290ef5edfe0db668225264335d3600bd281
Keith Booher <keith.booher@yahoo.com>
2018-07-30 01:49:10 -0500

    card logic

--- assets/javascript/bio.js
+++ assets/javascript/bio.js
@@ -1,8 +1,10 @@
 // click handlers for nav bar items to take us to the next carousel
 $(".portfolio").on("click", function(event) {
     console.log("stuff")
-    $(".carousel").carousel(1);
+
     $('.card').hide();
+
+    $(".carousel").carousel(1);
 
     var myVar = setInterval(myTimer, 500);
 

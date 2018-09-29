cd38401f8e023a4d78ae913a349233a9d7b30dd6
Keith Booher <keith.booher@yahoo.com>
2018-09-29 17:07:07 -0500

    updated click handlers

--- assets/javascript/bio.js
+++ assets/javascript/bio.js
@@ -1,63 +1,81 @@
 // click handlers for nav bar items to take us to the next carousel
 $(".portfolio").on("click", function(event) {
-    console.log("stuff")
+  console.log("stuff");
 
-    $('.card').hide();
+  $(".card").hide();
 
-    $(".carousel").carousel(1);
+  $(".carousel").carousel(1);
 
-    var myVar = setInterval(myTimer, 500);
+  var myVar = setInterval(myTimer, 500);
 
-    function myTimer() {
-        window.location.replace('portfolio.html')
-    }
-})
-
-
-$('.index').click(function() {
-    console.log("stuff2")
-
-    $(".carousel").carousel(0);
-    
-    var myVar = setInterval(myTimer, 500);
-
-    function myTimer() {
-        window.location.replace('index.html')
-    }
+  function myTimer() {
+    window.location.replace("portfolio.html");
+  }
 });
 
+$(".index").click(function() {
+  console.log("stuff2");
 
+  $(".carousel").carousel(0);
+
+  var myVar = setInterval(myTimer, 500);
+
+  function myTimer() {
+    window.location.replace("index.html");
+  }
+});
 
 //prevent carousel front auto sliding
-$('.carousel').carousel({
-    interval: false
+$(".carousel").carousel({
+  interval: false
 });
 
-
-
-
-document.getElementsByClassName("avatar").onclick = function () {
-    console.log('test')
-    location.href = "https://keithbooher.github.io/attack-based-game/";
+document.getElementById("avatar").onclick = function() {
+  console.log("test");
+  location.href = "https://keithbooher.github.io/attack-based-game/";
 };
 
-document.getElementsByClassName("camping").onclick = function () {
-    location.href = "https://iamstu.github.io/group-project1/";
+document.getElementById("avatarOther").onclick = function() {
+  console.log("test");
+  location.href = "https://keithbooher.github.io/attack-based-game/";
 };
 
-document.getElementsByClassName("reactScrape").onclick = function () {
-    location.href = "https://hidden-harbor-93880.herokuapp.com/";
+document.getElementById("camping").onclick = function() {
+  location.href = "https://iamstu.github.io/group-project1/";
 };
 
-document.getElementsByClassName("clicky").onclick = function () {
-    location.href = "https://evening-sea-23536.herokuapp.com/";
+document.getElementById("campingOther").onclick = function() {
+  location.href = "https://iamstu.github.io/group-project1/";
 };
 
-document.getElementsByClassName("artGutter").onclick = function () {
-    location.href = "https://artgutter.herokuapp.com/";
+document.getElementById("reactScrape").onclick = function() {
+  location.href = "https://hidden-harbor-93880.herokuapp.com/";
 };
 
-document.getElementsByClassName("nap").onclick = function () {
-    location.href = "https://stormy-retreat-21451.herokuapp.com/";
+document.getElementById("reactScrapeOther").onclick = function() {
+  location.href = "https://hidden-harbor-93880.herokuapp.com/";
 };
 
+document.getElementById("clicky").onclick = function() {
+  location.href = "https://evening-sea-23536.herokuapp.com/";
+};
+
+document.getElementById("clickyOther").onclick = function() {
+  location.href = "https://evening-sea-23536.herokuapp.com/";
+};
+
+document.getElementById("artGutter").onclick = function() {
+  location.href = "https://artgutter.herokuapp.com/";
+};
+
+document.getElementById("artGutterOther").onclick = function() {
+  location.href = "https://artgutter.herokuapp.com/";
+};
+
+document.getElementById("nap").onclick = function() {
+  location.href = "https://vast-cliffs-34645.herokuapp.com/";
+};
+
+document.getElementById("napOther").onclick = function() {
+  location.href = "https://vast-cliffs-34645.herokuapp.com/";
+};
